fad9a7ffb064dba996f249ab3690663d9b994fdd
alu0100693737 <alu0100693737@ull.edu.es>
2016-04-14 02:05:26 +0200

    añadido localstorage, ruta botones y empezando drapanddrop

--- public/js/csv.js
+++ public/js/csv.js
@@ -30,19 +30,47 @@ const dump = (fileName) => {
   });
 };
 
+//File
 const handleFileSelect = (evt) => {
+  evt.stopPropagation();
+  evt.preventDefault();
 
+  var files = evt.target.files;
+
+   var reader = new FileReader();
+   reader.onload = (e) => {
+
+     $("#original").val(e.target.result);
+   };
+   reader.readAsText(files[0])
 }
 
+//File
 /* Drag and drop: el fichero arrastrado se vuelca en la textarea de entrada */
 const handleDragFileSelect = (evt) => {
+  evt.stopPropagation();
+  evt.preventDefault();
 
+  var files = evt.dataTransfer.files; // FileList object.
+
+  var reader = new FileReader();
+  reader.onload = (e) => {
+
+    $("#original").val(e.target.result);
+    evt.target.style.background = "white";
+  };
+  reader.readAsText(files[0])
 }
+
 
 const handleDragOver = (evt) => {
-
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.target.style.background = "pink";
 }
 
+//localstorage, inicializacion de ruta botones,
+//añadidos eventos drap and drop
 $(document).ready(() => {
     let original = document.getElementById("original");
     if (window.localStorage && localStorage.original) {
@@ -59,10 +87,16 @@ $(document).ready(() => {
         );
    });
    /* botones para rellenar el textarea */
-
+   $('button.example').each( (_,y) => {
+     $(y).click( () => { dump(`examples/${$(y).text()}.txt`); });
+   });
 
     // Setup the drag and drop listeners.
     //var dropZone = document.getElementsByClassName('drop_zone')[0];
-
-
+    let dropZone = $('.drop_zone')[0];
+    dropZone.addEventListener('dragover', handleDragOver, false);
+    dropZone.addEventListener('drop', handleDragFileSelect, false);
+    let inputFile = $('.inputfile')[0];
+    inputFile.addEventListener('change', handleFileSelect, false);
+ });
 })();

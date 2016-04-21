678db795f9909cef8e613df1839d0d61c264074c
alu0100693737 <alu0100693737@ull.edu.es>
2016-04-21 10:39:36 +0000

    Problemas con los middleswares

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
-
 var producto = require('./controllers/producto');
-const Entrada = require('./controllers/db');
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/prueba');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -31,20 +26,26 @@ app.get('/csv', (request, response) => {
 
 app.get('/tutu', producto.index);
 
-app.get('/mongo/', function(req, res) {
+
+app.param('entrada', function(req, res, next, entrada) {
+  if (entrada.match(/^[a-z_]\w*\.csv$/i)) {
+    req.Entrada = entrada;
+  } else {
+    next(new Error(`<${entrada}> no casa con los requisitos de 'entrada'`));
+  }
+  next();
+});
+
+const Entrada = require('./models/db');
+
+app.get('/mongo/:entrada', function(req, res) {
   console.log(req.Entrada);
 
-  Entrada.find({}, function(err, docs) {
-        if (err)
-            return err;
-        if (docs.length >= 4) { //restringir a 4 archivos, el ultimo se podrá cambiar por el usuario
-            Entrada.find({ name: docs[3].name }).remove().exec();
-        }
-    });
+  mongoose.connect('mongodb://localhost/prueba');
 
   let input = new Entrada({
-    "name": req.params.Entrada,
-    "content": req.query.content
+    "name": req.Entrada,
+    "content": "test"
   });
 
   let promesa = Entrada.save(function(err) {
@@ -62,20 +63,10 @@ app.get('/mongo/', function(req, res) {
 });
 
 app.get('/find', function(req, res) {
-   Entrada.find({}, function(err, docs) {
-        if (err)
-            return err;
-        res.send(docs);
-    });
+  if (Entrada.find()[req.query.indice] != null)
+    req.query.elemento.toggleClass('input');
 });
-//se devuelve la entrada del nombre especificado en el request
-app.get('/findByName', function(req, res) {
-    Entrada.find({
-        name: req.query.name
-    }, function(err, docs) {
-        res.send(docs);
-    });
-});
+
 /*// Routes
 app.get('/', producto.index)
  
